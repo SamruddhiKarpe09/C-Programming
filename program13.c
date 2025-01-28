9c014b4eb25a5af976208d86864272bfa591a2b6
Samruddhi Ajit Karpe <114747665+SamruddhiKarpe09@users.noreply.github.com>
2025-01-28 14:20:03 +0530

    Add files via upload

---
+++ program13.c
@@ -0,0 +1,33 @@
+//Take a input from user a number and display Jay Ganesh that number of times
+
+
+#include<stdio.h>
+
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    if(iNo < 0)     // Filter
+    {
+        printf("Please enter the positive number\n");
+        return;
+    }
+
+    //      1          2        3
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("Jay Ganesh...\n");      // 4
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter the frequency of Jay Ganesh statement : \n");
+    scanf("%d",&iValue);
+
+    Display(iValue);
+    
+    return 0;
+}
