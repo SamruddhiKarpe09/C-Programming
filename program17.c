aef32ef4b82d6b8ca1252e7db1262bcf9243c609
Samruddhi Ajit Karpe <114747665+SamruddhiKarpe09@users.noreply.github.com>
2025-01-31 15:26:45 +0530

    Add files via upload

---
+++ program17.c
@@ -0,0 +1,20 @@
+//Display 1 to 5 numbers using a function
+
+#include<stdio.h>
+
+void Display()
+{
+    int iCnt = 0;
+
+    for(iCnt = 1; iCnt <= 5; iCnt++)
+    {
+        printf("%d\n",iCnt);
+    }
+}
+
+int main()
+{
+    Display();
+
+    return 0;
+}
